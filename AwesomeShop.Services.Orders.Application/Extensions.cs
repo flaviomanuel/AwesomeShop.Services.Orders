118aa9bd28524fe46b53910a2990d6feaa94cfdd
Flávio Manuel <flaviomanuel258@gmail.com>
2023-05-01 22:07:13 -0300

    feat: implement cqrs with commands and queries

---
+++ AwesomeShop.Services.Orders.Application/Extensions.cs
@@ -0,0 +1,15 @@
+using AwesomeShop.Services.Orders.Application.Commands;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AwesomeShop.Services.Orders.Application
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddHandlers(this IServiceCollection services){
+            services.AddMediatR(typeof(AddOrder));
+
+            return services;
+        }
+    }
+}
