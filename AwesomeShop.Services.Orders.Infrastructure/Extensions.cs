04ad054bae640d35f1bd9a5e1213642cfc87f2f8
Flávio Manuel <flaviomanuel258@gmail.com>
2023-05-02 07:51:50 -0300

    feat: MongoDb configuration

---
+++ AwesomeShop.Services.Orders.Infrastructure/Extensions.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AwesomeShop.Services.Orders.Core.Repositories;
+using AwesomeShop.Services.Orders.Infrastructure.Persistence;
+using AwesomeShop.Services.Orders.Infrastructure.Persistence.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace AwesomeShop.Services.Orders.Infrastructure
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddMongo(this IServiceCollection services){
+            
+            services.AddSingleton(s => {
+                var configuration = s.GetService<IConfiguration>();
+
+                var options = new MongoDbOptions();
+
+                configuration.GetSection("Mongo").Bind(options);
+
+                return options;
+
+            });
+
+            services.AddSingleton<IMongoClient>(sp => {
+                var options = sp.GetService<MongoDbOptions>();
+
+                return new MongoClient(options.ConnectionString);
+            });
+
+            services.AddTransient(sp => {
+                BsonDefaults.GuidRepresentation = GuidRepresentation.Standard;
+
+
+                var options = sp.GetService<MongoDbOptions>();
+                var mongoClient = sp.GetService<IMongoClient>();  
+
+                return mongoClient.GetDatabase(options.Database);
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddRepositories(this IServiceCollection services){
+
+            services.AddScoped<IOrderRepository, OrderRepository>();
+
+            return services;
+        }
+    }
+}
